b7b878b2317c5a0709da444c775a529957101686
Daniel Vasquez <dani.vasquez.dev@gmail.com>
2022-01-13 19:19:38 -0600

    Starting StartBusiness

--- src/components/StartBusiness.jsx
+++ src/components/StartBusiness.jsx
@@ -1,9 +1,22 @@
+import Btn from "./Btn";
+
 function StartBusiness() {
   return (
     <div>
-      Start growing your Business
+      <section>
+        Start <strong>growing</strong> your bussiness.
+        <p>
+          Your issue is our main priority. Our 24/7 support team is here to help
+          you and make sure our product is up to date. Have a presale question
+          about our products and features? Or looking for a refund? We would
+          love to hear what you concern is. Online awards and publications.
+        </p>
+      </section>
+      <section>
+        <Btn title='GET STARTED NOW!' color='cyan'/>
+      </section>
     </div>
-  )
+  );
 }
 
-export default StartBusiness
+export default StartBusiness;
