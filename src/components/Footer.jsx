f3958a5adfa4c0d4ec0c9283eb5a7461bc589057
Daniel Vasquez <dani.vasquez.dev@gmail.com>
2022-01-13 20:36:33 -0600

    Finishing the Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,54 @@
+import twitter from '../Assets/social/twitter.png'
+import facebook from '../Assets/social/facebook.png'
+import linkedin from '../Assets/social/linkedin.png'
+
+
+import './styles/Footer.css'
+
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className='Footer'>
+      <section className='Footer_duty'>
+        <p className='Footer_duty-title'>LATERAL</p>
+        <p className='Footer_duty-text'> Copryright c 2018 LATERAL.</p>
+        <p className='Footer_duty-text'>All Rights Reserved. Proudly mande in EU.</p>
+        <div className='Footer_duty-icons'>
+          <img src={twitter} alt="" />
+          <img src={facebook} alt="" />
+          <img src={linkedin} alt="" />
+        </div>
+      </section>
+      <section className='Footer_contact'>
+        <div>
+          <p className='Footer_contact-title'>Coompany</p>
+          <ul className='Footer_contact-ul'>
+            <li>About</li>
+            <li>Carrers</li>
+            <li>Awards</li>
+            <li>Users Program</li>
+            <li>Locations</li>
+          </ul>
+        </div>
+        <div>
+          <p className='Footer_contact-title'>Products</p>
+          <ul className='Footer_contact-ul'>
+            <li>Integrations</li>
+            <li>API</li>
+            <li>Pricing</li>
+            <li>Documentation</li>
+            <li>Release Notes</li>
+          </ul>
+        </div>
+        <div>
+          <p className='Footer_contact-title'>Support</p>
+          <ul className='Footer_contact-ul'>
+            <li>Contact</li>
+            <li>FAQ</li>
+            <li>Press</li>
+          </ul>
+        </div>
+      </section>
+    </footer>
   )
 }
 
--- src/components/StartBusiness.jsx
+++ src/components/StartBusiness.jsx
@@ -1,18 +1,19 @@
 import Btn from "./Btn";
+import './styles/StartBusiness.css'
 
 function StartBusiness() {
   return (
-    <div>
-      <section>
-        Start <strong>growing</strong> your bussiness.
-        <p>
+    <div className='StartBusiness'>
+      <section className='StartBusiness_text'>
+        <p className='StartBusiness_text-title'>Start <strong>growing</strong> your bussiness.</p>
+        <p className='StartBusiness_text-text'>
           Your issue is our main priority. Our 24/7 support team is here to help
           you and make sure our product is up to date. Have a presale question
           about our products and features? Or looking for a refund? We would
           love to hear what you concern is. Online awards and publications.
         </p>
       </section>
-      <section>
+      <section className='StartBusiness_btn'>
         <Btn title='GET STARTED NOW!' color='cyan'/>
       </section>
     </div>
